eb62be1e4956cdfedb9090e7733d84f5d8377087
surbhidavara99 <surabhidavara99@gmail.com>
2021-08-24 16:02:04 +0530

    Remove splash screen package

--- App.js
+++ App.js
@@ -5,15 +5,10 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
 Icon.loadFont();
-import SplashScreen from 'react-native-splash-screen';
 import RouteApp from './src/AppNavigator/Router';
 
 const Tab = createBottomTabNavigator();
 const App = () => {
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
   return (
     <>
       <NavigationContainer>
